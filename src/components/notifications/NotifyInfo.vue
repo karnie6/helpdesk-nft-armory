3f7b036fb34b7cef1106149598c5547563a2f286
Karthik Senthil <karthiksenthil@Prushas-MacBook-Air.local>
2022-02-16 11:31:27 -0500

    updating UX slightly

--- src/components/notifications/NotifyInfo.vue
+++ src/components/notifications/NotifyInfo.vue
@@ -1,13 +1,13 @@
 <template>
   <div v-if="isQuestion" style="padding: 16px;">
-    <h4>Question Submitted</h4>
+    <h4 v-if="isLoading && !isCreated">Question Submitted</h4>
     <span v-if="isLoading && !isCreated">Generating a NFT (this may take a minute)...</span>
     <div v-if="isLoading && isCreated">
     <span>GMNH Question NFT created! </span><ExplorerLink :mint-id="mintId" />
     </div>
   </div>
   <div v-else style="padding: 16px;">
-    <h4>Answer Submitted</h4>
+    <h4 v-if="isLoading && !isCreated">Answer Submitted</h4>
     <span v-if="isLoading && !isCreated">Generating a NFT (this may take a minute)...</span>
     <div v-if="isLoading && isCreated">
     <span>GMNH Answer NFT created! </span><ExplorerLink :mint-id="mintId" />
